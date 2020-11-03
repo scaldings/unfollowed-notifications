bf3b2d09645c782db135f240d29367875718aab5
Lucas <lukas.zahoransky@zsbystricany.info>
2020-11-03 15:26:22 +0100

    Switched from instaloader to instabot.

--- unfollowers.py
+++ unfollowers.py
@@ -1,31 +1,27 @@
-import instaloader
+from instabot import Bot
+from time import sleep
 import send_email
 from os import environ
 
 
-def get_loader(login: str, password: str):
-    loader = instaloader.Instaloader()
-    loader.login(login, password)
-    return loader
+def get_following():
+    following_list = []
+    bot = Bot()
+    bot.login(username=environ['LOGIN'], password=environ['PASSWORD'])
+    following = bot.get_user_following(environ['LOGIN'])
+    for flwing in following:
+        following_list.append(bot.get_username_from_user_id(flwing))
+    return following_list
 
 
-def get_profile(username: str, loader):
-    profile = instaloader.Profile.from_username(loader.context, username)
-    return profile
-
-
-def get_following(profile):
-    following = []
-    for following_user in profile.get_followees():
-        following.append(following_user)
-    return following
-
-
-def get_followers(profile):
-    followers = []
-    for follower in profile.get_followers():
-        followers.append(follower)
-    return followers
+def get_followers():
+    followers_list = []
+    bot = Bot()
+    bot.login(username=environ['LOGIN'], password=environ['PASSWORD'])
+    followers = bot.get_user_followers(environ['LOGIN'])
+    for follower in followers:
+        followers_list.append(bot.get_username_from_user_id(follower))
+    return followers_list
 
 
 def get_unfollowers(followers: list, following: list):
@@ -37,16 +33,16 @@ def get_unfollowers(followers: list, following: list):
 
 
 def main():
-    loader = get_loader(environ['LOGIN'], environ['PASSWORD'])
-    profile = get_profile(environ['LOGIN'], loader)
-    last_unfollowers = get_unfollowers(get_followers(profile), get_following(profile))
+    last_unfollowers = get_unfollowers(get_followers(), get_following())
     last_unfollowers_length = len(last_unfollowers)
     while True:
-        unfollowers = get_unfollowers(get_followers(profile), get_following(profile))
+        unfollowers = get_unfollowers(get_followers(), get_following())
         if last_unfollowers_length < len(unfollowers):
             for unfollower in unfollowers:
                 if unfollower not in last_unfollowers:
                     send_email.send_email(str(unfollower))
+        print('Sleeping for 5 minutes.')
+        sleep(300)
 
 
 if __name__ == '__main__':
